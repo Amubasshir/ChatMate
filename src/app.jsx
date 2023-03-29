399fcbdf5246143a83668036c89cb63385cf5818
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-03-29 16:10:37 +0600

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'preact/hooks';
+
+export function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>wellsdakjl</h1>
+      </div>
+    </>
+  );
+}
